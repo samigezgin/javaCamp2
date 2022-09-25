4abf475a7abc3d5c31ed05760c12da22bc0181b2
samigezgin <samigezgin@gmail.com>
2022-09-25 16:08:09 +0300

    hafta 2

---
+++ classes/src/classes/CustomerManager.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class CustomerManager {
+	
+	public void add() {
+		
+		System.out.println("Müşteri Eklendi ");
+	}
+
+	public void remove() {
+		
+		System.out.println("Müşteri silindi ");
+	}
+
+	public void update() {
+		
+		System.out.println("Müşteri güncellendi ");
+	}
+
+	
+}
