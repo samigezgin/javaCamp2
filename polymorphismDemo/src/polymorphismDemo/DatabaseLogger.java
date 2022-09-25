37e753f0b9455cd2d7dc864d9eaddf78410d61eb
samigezgin <samigezgin@gmail.com>
2022-09-25 18:30:17 +0300

    düzenlendi

--- polymorphismDemo/src/polymorphismDemo/DatabaseLogger.java
+++ polymorphismDemo/src/polymorphismDemo/DatabaseLogger.java
@@ -2,7 +2,7 @@ package polymorphismDemo;
 
 public class DatabaseLogger extends BaseLogger{
 	
-	public void Log(String message) {
+	public void log(String message) {
 		System.out.println("Logged to database : " +message);
 	}
 
